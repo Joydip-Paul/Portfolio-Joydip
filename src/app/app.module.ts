92d328465cbfb16ef84cb81c98a2bbfdabbef87d
Joydip Paul <joydippaul72@gmail.com>
2023-01-01 21:06:58 +0600

    Hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './module/hero/hero.component';
+import { AboutComponent } from './module/about/about.component';
+import { ProjectsComponent } from './module/projects/projects.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { SwiperModule } from 'swiper/angular';
+import { HeaderComponent } from './module/header/header.component';
+import { FooterComponent } from './module/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    AboutComponent,
+    ProjectsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/module/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
