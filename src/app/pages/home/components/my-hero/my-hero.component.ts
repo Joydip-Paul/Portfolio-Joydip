366fa14bded3e0b979df71cc60898bf0cef52cec
Joydip <joydip@Joydips-MacBook-Pro.local>
2023-07-23 21:47:17 +0600

    gsap

--- src/app/pages/home/components/my-hero/my-hero.component.ts
+++ src/app/pages/home/components/my-hero/my-hero.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-my-hero',
@@ -6,7 +14,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-hero.component.scss'],
 })
 export class MyHeroComponent implements OnInit {
+  @ViewChild('box1') box: ElementRef;
+  @ViewChildren('btn') btnContainers: QueryList<ElementRef>;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    gsap.from('.hero-content__left', { opacity: 0, duration: 1, y: -50 });
+
+    gsap.from('.hero-content__right', {
+      y: -1000,
+      x: 0,
+      duration: 0.8,
+      ease: 'bounce',
+      scale: 1.5,
+    });
+  }
 }
