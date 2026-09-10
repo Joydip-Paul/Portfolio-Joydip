84744d5ed06c704fda1eadc3d06403b197436359
joydip-paul <joydippaul72@gmail.com>
2026-09-11 02:11:00 +0600

    lenis added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import Lenis from 'lenis';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'portfolio';
   darkMode: boolean = false;
+  private lenis?: Lenis;
+  private animationFrameId?: number;
 
   constructor() { }
 
   ngOnInit(): void {
-
+    this.lenis = new Lenis({ autoRaf: false });
+    this.animationFrameId = window.requestAnimationFrame(this.raf);
   }
 
+  ngOnDestroy(): void {
+    if (this.animationFrameId !== undefined) {
+      window.cancelAnimationFrame(this.animationFrameId);
+    }
+
+    this.lenis?.destroy();
+  }
+
+  private raf = (time: number): void => {
+    this.lenis?.raf(time);
+    this.animationFrameId = window.requestAnimationFrame(this.raf);
+  };
 }
